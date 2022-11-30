fec2b5de5840f0b15a4d4cb4f47a9a21987777cf
jemand2001 <>
2022-11-30 21:36:00 +0100

    made the main functionality a lib for ease of use in each day's solution

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+use error_chain::error_chain;
+// use futures::executor::block_on;
+use reqwest::{self, header::HeaderMap};
+use std::{fs::{self, File, read_to_string}, path::{Path, PathBuf}, io::copy};
+use tokio::runtime::Builder;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        Http(reqwest::Error);
+    }
+}
+
+pub fn path_for(day: i32) -> PathBuf {
+    let p = Path::new("inputs/");
+    return p.join(day.to_string());
+}
+
+fn get_url(day: i32) -> String {
+    return String::from("https://adventofcode.com/2022/day/") + &day.to_string() + "/input"
+}
+
+async fn download(url: String, target: PathBuf) -> Result<()> {
+    let env_path = Path::new(".env");
+    if !env_path.exists() || !env_path.is_file() {
+        // return Err(Error(ErrorKind::Io(), "No cookie file found! (expected as .env)"));
+        panic!("No cookie file found! (expected as .env)");
+    }
+    let contents = read_to_string(env_path)?;
+    let cookie = contents.strip_suffix('\n').unwrap();
+    let mut headers = HeaderMap::new();
+    headers.insert("Cookie", cookie.parse().unwrap());
+    let client = reqwest::Client::new();
+    let response = client.get(url).headers(headers).send().await?;
+    let mut file = File::create(target)?;
+    let content = response.text().await?;
+    copy(&mut content.as_bytes(), &mut file)?;
+    Ok(())
+}
+
+fn get_input(day: i32, target: PathBuf) -> Result<()> {
+    Builder::new_current_thread()
+        .enable_io()
+        .enable_time()
+        .build()?
+        .block_on(download(get_url(day), target))
+}
+
+pub fn read_input(day: i32) -> Result<String> {
+    // Ok(String::new())
+    let path = path_for(day);
+    fs::create_dir_all(path.parent().unwrap().as_os_str()).expect("couldn't create inputs directory");
+    if path.exists() {
+        println!("Path already exists!");
+    }
+    else {
+        println!("Path doesn't already exist, downloading!");
+        get_input(day, path.to_owned())?;
+    }
+    let content = read_to_string(path)?;
+    Ok(content)
+}
+
+pub fn run_on_input<T, F, P>(day: i32, runner: F, parser: P) -> Result<()> where F : Fn(T), P : Fn(String) -> Result<T> {
+    let input = read_input(day)?;
+    let parsed = parser(input)?;
+    runner(parsed);
+    Ok(())
+}
